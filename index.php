ce8ad94de5dc1f19185384195eacf69d41e7125a
Hennedy <Kunaragi@users.noreply.github.com>
2016-11-24 10:06:14 +0100

    Justing adding something to correct at home..

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<header>
+<title>Cours PHP</title>
+<meta charset="utf-8">
+<link rel="stylesheet" media="screen" href="style.css">
+</header>
+
+<body>
+
+          <h1>Cours sur le PHP : Définition d'une boucle BUR en PHP</h1>
+<break />
+    <h2> Structure "for" </h2>
+
+  <xmp>
+ <?php
+ for($i=0; $i<=100){
+   echo("$i")
+   echo("_")
+ }
+ ?>
+      </xmp>
+
+
+
+</body>
+
+
+</html>
